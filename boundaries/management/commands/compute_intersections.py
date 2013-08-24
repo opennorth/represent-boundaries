0a77bad99758bc77140c6c6def4f8d5e68810367
Joshua Tauberer <jt@occams.info>
2013-08-24 09:56:26 -0400

    compute_intersections: add a JSON output format

--- boundaries/management/commands/compute_intersections.py
+++ boundaries/management/commands/compute_intersections.py
@@ -1,4 +1,4 @@
-import sys
+import sys, json
 
 from optparse import make_option
 
@@ -12,6 +12,13 @@ class Command(BaseCommand):
 	help = 'Create a report of the area of intersection of every pair of boundaries from two boundary sets specified by their slug.'
 	args = 'boundaryset1 boundaryset1'
 
+	option_list = BaseCommand.option_list + (
+		make_option('-f', '--format', action='store', dest='format', default="csv",
+			help='Choose an output format: csv, json.'),
+		make_option('-m', '--metadata', action='store_true', dest='include_metadata', default=False,
+			help='Includes the original shapefile metadata in the output.'),
+	)
+
 	def handle(self, *args, **options):
 		if len(args) < 2:
 			print "Specify two boundaryset slugs."
@@ -20,7 +27,10 @@ class Command(BaseCommand):
 		bset_a = BoundarySet.objects.get(slug=args[0])
 		bset_b = BoundarySet.objects.get(slug=args[1])
 
-		print bset_a.slug, "area_1", bset_b.slug, "area_2", "area_intersection", "pct_of_1", "pct_of_2"
+		if options["format"] == "csv":
+			print bset_a.slug, "area_1", bset_b.slug, "area_2", "area_intersection", "pct_of_1", "pct_of_2"
+		elif options["format"] == "json":
+			output = [ ]
 
 		# For each boundary in the first set...
 		for a_slug in bset_a.boundaries.order_by("slug").values_list('slug', flat=True):
@@ -47,4 +57,34 @@ class Command(BaseCommand):
 				if int_area/a_area < .001 or int_area/b_area < .001:
 					continue
 
-				print a_slug, a_area, b_bdry.slug, b_area, int_area, int_area/a_area, int_area/b_area
+				if options["format"] == "csv":
+					print a_slug, a_area, b_bdry.slug, b_area, int_area, int_area/a_area, int_area/b_area
+				elif options["format"] == "json":
+					output.append({
+						"area": int_area,
+						bset_a.slug: {
+							"id": a_bdry.external_id,
+							"name": a_bdry.name,
+							"slug": a_slug,
+							"centroid": tuple(a_bdry.centroid),
+							"extent": a_bdry.extent,
+							"area": a_area,
+							"ratio": int_area/a_area,
+						},
+						bset_b.slug: {
+							"id": b_bdry.external_id,
+							"name": b_bdry.name,
+							"slug": b_bdry.slug,
+							"centroid": tuple(b_bdry.centroid),
+							"extent": b_bdry.extent,
+							"area": b_area,
+							"ratio": int_area/b_area,
+						},
+					})
+					if options["include_metadata"]:
+						output[-1][bset_a.slug]["metadata"] = a_bdry.metadata
+						output[-1][bset_b.slug]["metadata"] = b_bdry.metadata
+				
+		if options["format"] == "json":
+			print json.dumps(output, sort_keys=True, indent=2)
+	
