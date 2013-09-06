3730170c08f4bb818ccaa8d4bcfc938b2e701f1f
Michael Mulley <michael@michaelmulley.com>
2013-09-06 15:06:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name = "represent-boundaries",
     version = "0.1",
     url='http://github.com/rhymeswithcycle/represent-boundaries',
+    description="A Web API to geographical districts loaded from shapefiles. Packaged as a Django app.",
     license = "MIT",
     packages = [
         'boundaries',
@@ -16,5 +17,11 @@ setup(
     install_requires = [
         'django-jsonfield>=0.7.1',
         'django-appconf',
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Topic :: Scientific/Engineering :: GIS',
     ]
 )
