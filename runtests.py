e020521e0099bc903c9b3aeaa22d07871e59d764
Michael Mulley <michael@michaelmulley.com>
2017-02-23 20:31:08 -0500

    Update test runner for Django 1.1 compatibility

--- runtests.py
+++ runtests.py
@@ -21,10 +21,19 @@ if not settings.configured:
             'django.contrib.gis',
             'boundaries',
         ),
-        TEMPLATES=[
+        TEMPLATES = [
             {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
                 'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
             },
         ],
     )
