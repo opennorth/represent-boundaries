11354a76710b71f78260d25dfc4fb3b96006024b
Michael Mulley <michael@michaelmulley.com>
2017-02-23 20:08:52 -0500

    Remove jsonfield import from old migrations

--- boundaries/migrations/0002_auto_20141129_1402.py
+++ boundaries/migrations/0002_auto_20141129_1402.py
@@ -2,8 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield.fields
 
+class JSONField(models.TextField):
+    """Mocks jsonfield 0.92's column-type behaviour"""
+    def db_type(self, connection):
+        if connection.vendor == 'postgresql' and connection.pg_version >= 90300:
+            return 'json'
+        else:
+            return super(JSONField, self).db_type(connection)
 
 def set_defaults(apps, schema_editor):
     Boundary = apps.get_model("boundaries", "Boundary")
@@ -24,11 +30,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='boundary',
             name='metadata',
-            field=jsonfield.fields.JSONField(default={}, help_text='The attributes of the boundary from the shapefile, as a dictionary.'),
+            field=JSONField(default={}, help_text='The attributes of the boundary from the shapefile, as a dictionary.'),
         ),
         migrations.AlterField(
             model_name='boundaryset',
             name='extra',
-            field=jsonfield.fields.JSONField(default={}, help_text='Any additional metadata.'),
+            field=JSONField(default={}, help_text='Any additional metadata.'),
         ),
     ]
