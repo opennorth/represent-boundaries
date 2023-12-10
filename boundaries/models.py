opennorth/represent-boundaries

commit 0b3a979905ed2834af1ce6d35986021207e084b8

chore: Fix E501

boundaries/models.py
@@ -9,8 +9,8 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.core.serializers.json import DjangoJSONEncoder
 from django.template.defaultfilters import slugify
 from django.urls import reverse
-from django.utils.translation import gettext as _
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
 
 
 class MyAppConf(AppConf):
@@ -42,70 +42,85 @@ class BoundarySet(models.Model):
         max_length=200,
         primary_key=True,
         editable=False,
-        help_text=gettext_lazy("The boundary set's unique identifier, used as a path component in URLs."),
+        help_text=_("The boundary set's unique identifier, used as a path component in URLs."),
     )
     name = models.CharField(
         max_length=100,
         unique=True,
-        help_text=gettext_lazy('The plural name of the boundary set.'),
+        help_text=_('The plural name of the boundary set.'),
     )
     singular = models.CharField(
         max_length=100,
-        help_text=gettext_lazy('A generic singular name for a boundary in the set.'),
+        help_text=_('A generic singular name for a boundary in the set.'),
     )
     authority = models.CharField(
         max_length=256,
-        help_text=gettext_lazy('The entity responsible for publishing the data.'),
+        help_text=_('The entity responsible for publishing the data.'),
     )
     domain = models.CharField(
         max_length=256,
-        help_text=gettext_lazy("The geographic area covered by the boundary set."),
+        help_text=_("The geographic area covered by the boundary set."),
     )
     last_updated = models.DateField(
-        help_text=gettext_lazy('The most recent date on which the data was updated.')),
+        help_text=_('The most recent date on which the data was updated.')),
     source_url = models.URLField(
         blank=True,
-        help_text=gettext_lazy('A URL to the source of the data.'),
+        help_text=_('A URL to the source of the data.'),
     )
     notes = models.TextField(
         blank=True,
-        help_text=gettext_lazy('Free-form text notes, often used to describe changes that were made to the original source data.'),
+        help_text=_(
+            'Free-form text notes, often used to describe changes that were made to the original source data.'
+        ),
     )
     licence_url = models.URLField(
         blank=True,
-        help_text=gettext_lazy('A URL to the licence under which the data is made available.'),
+        help_text=_('A URL to the licence under which the data is made available.'),
     )
     extent = models.JSONField(
         blank=True,
         null=True,
-        help_text=gettext_lazy("The set's boundaries' bounding box as a list like [xmin, ymin, xmax, ymax] in EPSG:4326."),
+        help_text=_("The set's boundaries' bounding box as a list like [xmin, ymin, xmax, ymax] in EPSG:4326."),
     )
     start_date = models.DateField(
         blank=True,
         null=True,
-        help_text=gettext_lazy("The date from which the set's boundaries are in effect."),
+        help_text=_("The date from which the set's boundaries are in effect."),
     )
     end_date = models.DateField(
         blank=True,
         null=True,
-        help_text=gettext_lazy("The date until which the set's boundaries are in effect."),
+        help_text=_("The date until which the set's boundaries are in effect."),
     )
     extra = models.JSONField(
         default=dict,
         blank=True,
-        help_text=gettext_lazy("Any additional metadata."),
+        help_text=_("Any additional metadata."),
     )
 
     name_plural = property(lambda s: s.name)
     name_singular = property(lambda s: s.singular)
 
-    api_fields = ('name_plural', 'name_singular', 'authority', 'domain', 'source_url', 'notes', 'licence_url', 'last_updated', 'extent', 'extra', 'start_date', 'end_date')
+    api_fields = [
+        'name_plural',
+        'name_singular',
+        'authority',
+        'domain',
+        'source_url',
+        'notes',
+        'licence_url',
+        'last_updated',
+        'extent',
+        'extra',
+        'start_date',
+        'end_date',
+    ]
     api_fields_doc_from = {'name_plural': 'name', 'name_singular': 'singular'}
 
     class Meta:
         ordering = ('name',)
-        verbose_name = gettext_lazy('boundary set')
-        verbose_name_plural = gettext_lazy('boundary sets')
+        verbose_name = _('boundary set')
+        verbose_name_plural = _('boundary sets')
 
     def __str__(self):
         return self.name
@@ -160,71 +175,73 @@ class Boundary(models.Model):
         BoundarySet,
         related_name='boundaries',
         on_delete=models.CASCADE,
-        help_text=gettext_lazy('The set to which the boundary belongs.'),
+        help_text=_('The set to which the boundary belongs.'),
     )
     set_name = models.CharField(
         max_length=100,
-        help_text=gettext_lazy('A generic singular name for the boundary.'),
+        help_text=_('A generic singular name for the boundary.'),
     )
     slug = models.SlugField(
         max_length=200,
         db_index=True,
-        help_text=gettext_lazy("The boundary's unique identifier within the set, used as a path component in URLs."),
+        help_text=_("The boundary's unique identifier within the set, used as a path component in URLs."),
     )
     external_id = models.CharField(
         max_length=255,
-        help_text=gettext_lazy("An identifier of the boundary, which should be unique within the set."),
+        help_text=_("An identifier of the boundary, which should be unique within the set."),
     )
     name = models.CharField(
         max_length=192,
         db_index=True,
-        help_text=gettext_lazy('The name of the boundary.'),
+        help_text=_('The name of the boundary.'),
     )
     metadata = models.JSONField(
         default=dict,
         blank=True,
         encoder=DjangoJSONEncoder,
-        help_text=gettext_lazy('The attributes of the boundary from the shapefile, as a dictionary.'),
+        help_text=_('The attributes of the boundary from the shapefile, as a dictionary.'),
     )
     shape = models.MultiPolygonField(
-        help_text=gettext_lazy('The geometry of the boundary in EPSG:4326.'),
+        help_text=_('The geometry of the boundary in EPSG:4326.'),
     )
     simple_shape = models.MultiPolygonField(
-        help_text=gettext_lazy('The simplified geometry of the boundary in EPSG:4326.'),
+        help_text=_('The simplified geometry of the boundary in EPSG:4326.'),
     )
     centroid = models.PointField(
         null=True,
-        help_text=gettext_lazy('The centroid of the boundary in EPSG:4326.'),
+        help_text=_('The centroid of the boundary in EPSG:4326.'),
     )
     extent = models.JSONField(
         blank=True,
         null=True,
-        help_text=gettext_lazy('The bounding box of the boundary as a list like [xmin, ymin, xmax, ymax] in EPSG:4326.'),
+        help_text=_('The bounding box of the boundary as a list like [xmin, ymin, xmax, ymax] in EPSG:4326.'),
     )
     label_point = models.PointField(
         blank=True,
         null=True,
         spatial_index=False,
-        help_text=gettext_lazy('The point at which to place a label for the boundary in EPSG:4326, used by represent-maps.'),
+        help_text=_('The point at which to place a label for the boundary in EPSG:4326, used by represent-maps.'),
     )
     start_date = models.DateField(
         blank=True,
         null=True,
-        help_text=gettext_lazy("The date from which the boundary is in effect."),
+        help_text=_("The date from which the boundary is in effect."),
     )
     end_date = models.DateField(
         blank=True,
         null=True,
-        help_text=gettext_lazy("The date until which the boundary is in effect."),
+        help_text=_("The date until which the boundary is in effect."),
     )
 
-    api_fields = ['boundary_set_name', 'name', 'metadata', 'external_id', 'extent', 'centroid', 'start_date', 'end_date']
+    api_fields = [
+        'boundary_set_name', 'name', 'metadata', 'external_id', 'extent', 'centroid', 'start_date', 'end_date'
+    ]
     api_fields_doc_from = {'boundary_set_name': 'set_name'}
 
     class Meta:
         unique_together = (('slug', 'set'))
-        verbose_name = gettext_lazy('boundary')
-        verbose_name_plural = gettext_lazy('boundaries')  # avoids "boundarys"
+        verbose_name = _('boundary')
+        verbose_name_plural = _('boundaries')  # avoids "boundarys"
 
     def __str__(self):
         return f"{self.name} ({self.set_name})"
@@ -324,7 +341,7 @@ class Geometry:
         Uses `ST_SimplifyPreserveTopology` to avoid invalid geometries and
         ensures the result is a MultiPolygon.
         """
-        geometry = self.geometry.geos.simplify(app_settings.SIMPLE_SHAPE_TOLERANCE, preserve_topology=True).ogr  # simplify is in GEOS
+        geometry = self.geometry.geos.simplify(app_settings.SIMPLE_SHAPE_TOLERANCE, preserve_topology=True).ogr
         geometry = self.geometry_to_multipolygon(geometry)  # simplify can return a Polygon
         return Geometry(geometry)
 
@@ -372,7 +389,9 @@ class Geometry:
             multipolygon.add(geometry)
             return multipolygon
         else:
-            raise ValueError(_('The geometry is a %(value)s but must be a Polygon or a MultiPolygon.') % {'value': value})
+            raise ValueError(
+                gettext('The geometry is a %(value)s but must be a Polygon or a MultiPolygon.') % {'value': value}
+            )
 
 
 slug_re = re.compile(r'[–—]')  # n-dash, m-dash

boundaries/base_views.py
@@ -179,7 +179,9 @@ class ModelGeoListView(ModelListView):
                     wkt = f'POINT({longitude} {latitude})'
                     qs = qs.filter(**{self.default_geo_filter_field + "__contains": wkt})
                 except ValueError:
-                    raise BadRequest(_("Invalid latitude,longitude '%(value)s' provided.") % {'value': request.GET['contains']})
+                    raise BadRequest(
+                        _("Invalid latitude,longitude '%(value)s' provided.") % {'value': request.GET['contains']}
+                    )
 
             if 'near' in request.GET:
                 latitude, longitude, range = request.GET['near'].split(',')
@@ -208,8 +210,10 @@ class ModelGeoListView(ModelListView):
 
         if qs.count() > app_settings.MAX_GEO_LIST_RESULTS:
             return HttpResponseForbidden(
-                _("Spatial-list queries cannot return more than %(expected)d resources; this query would return %(actual)s. Please filter your query.")
-                % {'expected': app_settings.MAX_GEO_LIST_RESULTS, 'actual': qs.count()})
+                _(
+                    "Spatial-list queries cannot return more than %(expected)d resources; "
+                    "this query would return %(actual)s. Please filter your query."
+                ) % {'expected': app_settings.MAX_GEO_LIST_RESULTS, 'actual': qs.count()})
 
         format = request.GET.get('format', 'json')
 
@@ -222,7 +226,9 @@ class ModelGeoListView(ModelListView):
             strings.append(']}')
             return RawJSONResponse(''.join(strings))
         elif format == 'wkt':
-            return HttpResponse("\n".join(geom.wkt for geom in qs.values_list(field, flat=True)), content_type="text/plain")
+            return HttpResponse(
+                "\n".join(geom.wkt for geom in qs.values_list(field, flat=True)), content_type="text/plain"
+            )
         elif format == 'kml':
             placemarks = [kml.generate_placemark(x[1], x[0]) for x in qs.values_list(field, self.name_field)]
             resp = HttpResponse(
@@ -306,7 +312,9 @@ class Paginator:
     Taken from django-tastypie. Thanks!
     """
 
-    def __init__(self, request_data, objects, resource_uri=None, limit=None, offset=0, max_limit=1000, collection_name='objects'):
+    def __init__(
+        self, request_data, objects, resource_uri=None, limit=None, offset=0, max_limit=1000, collection_name='objects'
+    ):
         """
         Instantiates the ``Paginator`` and allows for some configuration.
 
@@ -356,10 +364,14 @@ class Paginator:
         try:
             limit = int(limit)
         except ValueError:
-            raise BadRequest(_("Invalid limit '%(value)s' provided. Please provide a positive integer.") % {'value': limit})
+            raise BadRequest(
+                _("Invalid limit '%(value)s' provided. Please provide a positive integer.") % {'value': limit}
+            )
 
         if limit < 0:
-            raise BadRequest(_("Invalid limit '%(value)s' provided. Please provide a positive integer >= 0.") % {'value': limit})
+            raise BadRequest(
+                _("Invalid limit '%(value)s' provided. Please provide a positive integer >= 0.") % {'value': limit}
+            )
 
         if self.max_limit and (not limit or limit > self.max_limit):
             # If it's more than the max, we're only going to return the max.
@@ -385,10 +397,14 @@ class Paginator:
         try:
             offset = int(offset)
         except ValueError:
-            raise BadRequest(_("Invalid offset '%(value)s' provided. Please provide a positive integer.") % {'value': offset})
+            raise BadRequest(
+                _("Invalid offset '%(value)s' provided. Please provide a positive integer.") % {'value': offset}
+            )
 
         if offset < 0:
-            raise BadRequest(_("Invalid offset '%(value)s' provided. Please provide a positive integer >= 0.") % {'value': offset})
+            raise BadRequest(
+                _("Invalid offset '%(value)s' provided. Please provide a positive integer >= 0.") % {'value': offset}
+            )
 
         return offset
 

boundaries/management/commands/compute_intersections.py
@@ -8,7 +8,10 @@ from boundaries.models import BoundarySet
 
 
 class Command(BaseCommand):
-    help = _('Create a report of the area of intersection of every pair of boundaries from two boundary sets specified by their slug.')
+    help = _(
+        'Create a report of the area of intersection of every pair of boundaries from two boundary sets '
+        'specified by their slug.'
+    )
 
     def add_arguments(self, parser):
         parser.add_argument('slug', nargs=2)

boundaries/management/commands/loadshapefiles.py
@@ -70,7 +70,10 @@ class Command(BaseCommand):
             action='store',
             dest='merge',
             default=None,
-            help=_('Merge strategy when there are duplicate slugs, either "combine" (extend the MultiPolygon) or "union" (union the geometries).'),
+            help=_(
+                'Merge strategy when there are duplicate slugs, '
+                'either "combine" (extend the MultiPolygon) or "union" (union the geometries).'
+            ),
         )
 
     def get_version(self):
@@ -105,7 +108,11 @@ class Command(BaseCommand):
                 definition.setdefault('name', name)
                 definition = Definition(definition)
 
-                data_sources, tmpdirs = create_data_sources(definition['file'], encoding=definition['encoding'], convert_3d_to_2d=options['clean'])
+                data_sources, tmpdirs = create_data_sources(
+                    definition['file'],
+                    encoding=definition['encoding'],
+                    convert_3d_to_2d=options['clean'],
+                )
 
                 try:
                     if not data_sources:
@@ -179,7 +186,9 @@ class Command(BaseCommand):
         if None not in boundary_set.extent:  # unless there are no features
             boundary_set.save()
 
-        log.info(_('%(slug)s count: %(count)i') % {'slug': slug, 'count': Boundary.objects.filter(set=boundary_set).count()})
+        log.info(
+            _('%(slug)s count: %(count)i') % {'slug': slug, 'count': Boundary.objects.filter(set=boundary_set).count()}
+        )
 
     def load_boundary(self, feature, merge_strategy=None):
         if merge_strategy:
@@ -190,7 +199,9 @@ class Command(BaseCommand):
                 elif merge_strategy == 'union':
                     boundary.unary_union(feature.geometry)
                 else:
-                    raise ValueError(_("The merge strategy '%(value)s' must be 'combine' or 'union'.") % {'value': merge_strategy})
+                    raise ValueError(
+                        _("The merge strategy '%(value)s' must be 'combine' or 'union'.") % {'value': merge_strategy}
+                    )
                 boundary.centroid = boundary.shape.centroid
                 boundary.extent = boundary.shape.extent
                 boundary.save()
@@ -242,7 +253,9 @@ def create_data_sources(path, encoding='ascii', convert_3d_to_2d=False, zipfile=
         elif path.endswith('.zip'):
             return create_data_sources_from_zip(path)
         else:
-            raise ValueError(_("The path must be a shapefile, a ZIP file, or a directory: %(value)s.") % {'value': path})
+            raise ValueError(
+                _("The path must be a shapefile, a ZIP file, or a directory: %(value)s.") % {'value': path}
+            )
 
     data_sources = []
     tmpdirs = []

setup.cfg
@@ -31,10 +31,10 @@ line_length = 119
 profile = black
 
 [flake8]
+max-line-length = 119
 exclude = boundaries/migrations
-extend-ignore =
-    # E501 line too long (X > 79 characters)
-    E501
+per-file-ignores =
+    boundaries/tests/*: E501
 
 [coverage:report]
 omit =

commit 85758b85dec20be9dbb558f57d584a48911a064a

fix: Fix error introduced in 0b3a9799

boundaries/models.py
@@ -62,7 +62,8 @@ class BoundarySet(models.Model):
         help_text=_("The geographic area covered by the boundary set."),
     )
     last_updated = models.DateField(
-        help_text=_('The most recent date on which the data was updated.')),
+        help_text=_('The most recent date on which the data was updated.'),
+    )
     source_url = models.URLField(
         blank=True,
         help_text=_('A URL to the source of the data.'),