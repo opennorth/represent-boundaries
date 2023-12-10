6ae8b3ab612734bba9c05bfa2de519dfd4c8d946
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-10 00:43:13 -0500

    chore: Fix E124

--- boundaries/tests/definitions/no_features/definition.py
+++ boundaries/tests/definitions/no_features/definition.py
@@ -2,7 +2,8 @@ from datetime import date
 
 import boundaries
 
-boundaries.register('Districts',
+boundaries.register(
+    'Districts',
     last_updated=date(2000, 1, 1),
     name_func=boundaries.attr('id'),
     file='../../fixtures/foo.shp',
--- boundaries/tests/definitions/polygons/definition.py
+++ boundaries/tests/definitions/polygons/definition.py
@@ -2,7 +2,8 @@ from datetime import date
 
 import boundaries
 
-boundaries.register('Polygons',
+boundaries.register(
+    'Polygons',
     last_updated=date(2000, 1, 1),
     name_func=boundaries.attr('str'),
 )
